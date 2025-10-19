859c30796ab834c02cb1973cad7cff752996f6c3
Raulgastro <Gastroraul4@gmail.com>
2025-10-19 16:33:32 +0200

    Fix: suppression du mode export pour Render Web Service

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: "./",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
